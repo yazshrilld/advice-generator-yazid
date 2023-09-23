b1efcb9ab7173ce7d2c0a7f189e61bf712c93e21
yazshrilld <yazshrilld@gmail.com>
2023-09-23 19:27:32 +0100

    code update

--- js/script.js
+++ js/script.js
@@ -1,23 +1,12 @@
-// alert("i am here")
-
-const diceBtn = document.getElementById("dicebtn");
-// let loading = document.getElementById("preloade");
 const adviceContainer = document.querySelector(".text");
 const idContainer = document.querySelector(".span");
 const getAdvice = document.querySelector(".dice");
-const toggle = document.querySelector(".loads")
+const toggle = document.querySelector(".loads");
 let loaders = false;
 async function fetchAdvice() {
   loaders = true;
-  // if (loaders) {
-  //   // window.addEventListener("load", function () {
-  //     const myLoader = document.querySelector("preloader");
-  //     console.log(myLoader);
-  //   // });
-  // }
   console.log({ loaders });
-  // loaders ? toggle.classList.remove("preloader") : toggle.classList.add("hidden")
-  loaders ? toggle.classList.add("preloader") : ""
+  loaders ? toggle.classList.add("preloader") : "";
   const response = await fetch("https://api.adviceslip.com/advice");
   const movies = await response.json();
 
@@ -26,7 +15,7 @@ async function fetchAdvice() {
   const advId = movies["slip"]["id"];
 
   loaders = "false";
-  loaders ? toggle.classList.remove("preloader") : ""
+  loaders ? toggle.classList.remove("preloader") : "";
   console.log("state:", loaders);
   return { advice, advId };
 }
